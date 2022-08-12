df6735e6e21e84472223ab8b9a9b7d571d048473
honyava <88489271+honyava@users.noreply.github.com>
2022-08-12 23:25:24 +0300

    lcd
    lcd

---
+++ CMSIS_Project_stm32F407VET6/MDK-ARM/lcd_ili9341.c
@@ -0,0 +1,67 @@
+#include "lcd_ili9341.h"
+
+void writeLCDCommand(unsigned int reg,unsigned int value)
+{
+    *(uint16_t *) (LCD_REG) = reg;
+    *(uint16_t *) (LCD_DATA) = value;
+}
+void writeLCDData(unsigned int data)
+{
+    *(uint16_t *) (LCD_DATA)= data;
+}
+
+void initLCD()
+{
+//    // ?????????? Reset ???????
+//    GPIO_ResetBits(GPIOE, GPIO_Pin_1);
+//    HAL_Delay(0x0FFFFF);
+//    GPIO_SetBits(GPIOE, GPIO_Pin_1);
+//    delay(0x0FFFFF);
+ 
+    writeLCDCommand(0x0000,0x0001);
+    HAL_Delay(10);
+    writeLCDCommand(0x0015,0x0030);
+    writeLCDCommand(0x0011,0x0040);
+    writeLCDCommand(0x0010,0x1628);
+    writeLCDCommand(0x0012,0x0000);
+    writeLCDCommand(0x0013,0x104d);
+    HAL_Delay(10);
+    writeLCDCommand(0x0012,0x0010);
+    HAL_Delay(10);
+    writeLCDCommand(0x0010,0x2620);
+    writeLCDCommand(0x0013,0x344d);
+    HAL_Delay(10);
+    writeLCDCommand(0x0001,0x0100);
+    writeLCDCommand(0x0002,0x0300);
+    writeLCDCommand(0x0003,0x1030);
+    writeLCDCommand(0x0008,0x0604);
+    writeLCDCommand(0x0009,0x0000);
+    writeLCDCommand(0x000A,0x0008);
+    writeLCDCommand(0x0041,0x0002);
+    writeLCDCommand(0x0060,0x2700);
+    writeLCDCommand(0x0061,0x0001);
+    writeLCDCommand(0x0090,0x0182);
+    writeLCDCommand(0x0093,0x0001);
+    writeLCDCommand(0x00a3,0x0010);
+    HAL_Delay(10);
+    // ????????? ?????
+    writeLCDCommand(0x30,0x0000);
+    writeLCDCommand(0x31,0x0502);
+    writeLCDCommand(0x32,0x0307);
+    writeLCDCommand(0x33,0x0305);
+    writeLCDCommand(0x34,0x0004);
+    writeLCDCommand(0x35,0x0402);
+    writeLCDCommand(0x36,0x0707);
+    writeLCDCommand(0x37,0x0503);
+    writeLCDCommand(0x38,0x1505);
+    writeLCDCommand(0x39,0x1505);
+    HAL_Delay(10);
+    writeLCDCommand(0x0007,0x0001);
+    HAL_Delay(10);
+    writeLCDCommand(0x0007,0x0021);
+    writeLCDCommand(0x0007,0x0023);
+    HAL_Delay(10);
+    writeLCDCommand(0x0007,0x0033);
+    HAL_Delay(10);
+    writeLCDCommand(0x0007,0x0133);
+}
---
+++ CMSIS_Project_stm32F407VET6/MDK-ARM/lcd_ili9341.h
@@ -0,0 +1,16 @@
+#ifndef LCD_ILI9341_H
+#define LCD_ILI9341_H
+#include "stm32f4xx_hal.h"
+
+/***************************************************************************************/
+// To define address where we will write data
+// For writing data
+#define LCD_DATA                                                 ((uint32_t)0x60020000)
+// For writing commands
+#define LCD_REG                                                  ((uint32_t)0x60000000)
+/***************************************************************************************/
+void writeLCDCommand(unsigned int reg,unsigned int value);
+void writeLCDData(unsigned int data);
+void initLCD(void);
+
+#endif
